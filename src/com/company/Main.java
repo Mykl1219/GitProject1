ecbcf42290acfd6b822e4602108cb01b1108f652
Michael <Mykl2732@aol.com>
2022-05-14 12:03:09 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I am groot!!!");
+        System.out.println("I am groot2!!!");
+        System.out.println("I am groot3!!!");
+    }
 }
